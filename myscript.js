cb303767ad0db5626623f1a3c64d45e15fe4f347
Rajeev Ranjan <rajeevr405@outlook.com>
2021-07-18 20:13:06 +0530

    add request node module

--- myscript.js
+++ myscript.js
@@ -1,6 +1,16 @@
 function results() {
-    var url = document.getElementById("inputurl").value;
-    // document.write(url);
-    document.getElementById("shorturl").innerHTML =
-        "URL entered by you is " + url;
+  var url = document.getElementById("inputurl").value;
+  // document.write(url);
+  // document.getElementById(
+  //   "shorturl"
+  // ).innerHTML = `URL entered by you is ${url}`;
+  console.log(url); //For debug
 }
+
+var requests = require('request')
+var headers = {
+  'Authorization': 'c531e773c3ee0a030671198ad503b16e4e214ada',
+  'Content-Type' : 'application/json'
+};
+
+var dataString = '{ "long_url": url, }'
