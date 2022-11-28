8de5aeed1a681526122fd61a2b9138e398ea6e4d
zootedb0t <05kraken@proton.me>
2022-11-28 11:53:58 +0530

    remove virtual env from git

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # url_shortner
-The url-shortner is using Bitly api for url-shortening.
-
-![Image of Web-Page](https://github.com/zootedb0t/url_shortner/blob/main/resource/readme.png)
+A url-shortner built using Bitly api for url-shortening.
--- shortner/routes.py
+++ shortner/routes.py
@@ -1,6 +1,26 @@
 from shortner import app
-from flask import render_template, request
+from flask import render_template
+from flask import request
+import requests
+import json
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/shortner', methods=['POST'])
+def shortner():
+    url = request.form['data']
+    url_secure = "https://" + url
+    headers = {
+        'Authorization': '9a02ca6a0051dc7d0af541e95e6f6c1f26704c2a',
+        'Content-Type': 'application/json',
+    }
+    data = '{ "long_url": "https://www.google.com", "group_guid": "Bl7ibQEpEmV" }'
+    response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
+    return response.json()
+    # value = response.json()
+    # json_format = json.dump(value, indent=2)
+    # return json.load(json_format)
+    # json_output = json.load(json_format)
+    # return json.dumps(response.json(), indent=2)
