7171c2e350148afdd81ba7c319a35044a1a6a457
zootedb0t <05kraken@proton.me>
2022-12-01 15:08:30 +0530

    add qr-code

--- shortner/routes.py
+++ shortner/routes.py
@@ -5,13 +5,16 @@ from flask import render_template, request, redirect
 import requests
 import json
 
+
 @app.before_first_request
 def create_tables():
     db.create_all()
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/shortner", methods=["POST"])
 def shortner():
@@ -21,7 +24,7 @@ def shortner():
     else:
         url = "https://" + form_url
     headers = {
-        "Authorization": "your auth key",
+        "Authorization": "yout auth key",
         "Content-Type": "application/json",
     }
     raw_data = {"long_url": url, "group_guid": "your group id"}
@@ -33,26 +36,48 @@ def shortner():
     short_link = json.loads(format)["link"]
 
     # Check for duplicates
-    if form_url != '' and short_link != '':
+    if form_url != "" and short_link != "":
         p = Url(actual_url=form_url, short_url=short_link)
         db.session.add(p)
         db.session.commit()
-    return render_template("slink.html", link = short_link )
+    return render_template("slink.html", link=short_link)
+
 
 @app.route("/database")
 def get_url():
-    conn = sqlite3.connect('your database')
+    conn = sqlite3.connect(
+        "your database"
+    )
     cur = conn.cursor()
-    url = cur.execute('SELECT * FROM url').fetchall()
+    url = cur.execute("SELECT * FROM url").fetchall()
     conn.close()
     return render_template("database.html", url=url)
 
-@app.route('/delete/<int:id>')
+
+@app.route("/qrcode/<int:id>")
+def getqr(id):
+    headers = {
+        "Authorization": "your auth key",
+    }
+    params = (("image_format", "svg"),)
+    data = Url.query.filter_by(id=id).first()
+    url_secure = data.short_url
+    db.session.commit()
+    bit_url = url_secure.removeprefix("https://")
+    r = f"https://api-ssl.bitly.com/v4/bitlinks/{bit_url}/qr"
+    response = requests.get(r, headers=headers, params=params).json()
+    format = json.dumps(response, indent=2)
+    api_output = json.loads(format)["description"]
+    return api_output
+
+
+@app.route("/delete/<int:id>")
 def erase(id):
     data = Url.query.get(id)
     db.session.delete(data)
     db.session.commit()
-    return redirect('/')
+    return redirect("/")
+
 
 @app.errorhandler(500)
 def basic_error(e):
