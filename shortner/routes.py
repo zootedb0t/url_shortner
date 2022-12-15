9e3ce3ee7d29915fdce01f6690a524de776f7078
zootedb0t <05kraken@proton.me>
2022-12-15 11:21:19 +0530

    flash message working

--- shortner/routes.py
+++ shortner/routes.py
@@ -1,7 +1,7 @@
 from shortner import app
 from shortner.model import Url, db
 import sqlite3
-from flask import render_template, request, redirect
+from flask import flash, render_template, request, redirect, make_response
 import requests
 import json
 import pyperclip
@@ -20,6 +20,7 @@ def home():
     return render_template("index.html")
 
 
+# Authkey and group-id is stored in api.py file
 @app.route("/shortner", methods=["POST", "GET"])
 def shortner():
     if request.method == "POST":
@@ -52,7 +53,9 @@ def shortner():
 
 @app.route("/database")
 def database():
-    conn = sqlite3.connect("your database")
+    conn = sqlite3.connect(
+            "your database"
+    )
     cur = conn.cursor()
     url = cur.execute("SELECT * FROM url").fetchall()
     conn.close()
@@ -88,8 +91,16 @@ def copytoclipboard(id):
     data = Url.query.filter_by(id=id).first()
     copy = data.short_url
     pyperclip.copy(copy)
+    flash("Copied to clipboard")
     return redirect("/database")
 
+
+# Playing with cookie
+# @app.route("/setcookie")
+# def setcookie():
+#     response = make_response('<h1>Say hello to cookie!</h1>')
+#     response.set_cookie('message', 'I\'m your cookie')
+#     return response
 
 @app.errorhandler(500)
 def basic_error(e):
--- shortner/static/myscript.js
+++ shortner/static/myscript.js
@@ -1,13 +1,15 @@
-// WIP copy url to database
-getrow = function(val) {
-  db.transaction(function(transaction) {
-    transaction.executeSql("SELECT * FROM url where id =" + parseInt(val, 10)) +
-      ";";
-  });
-};
-
 function urlCopy(e) {
   const cb = navigator.clipboard;
   const data = document.querySelector(e);
   cb.writeText(data.innerText).then(() => alert("URL copied"));
 }
+
+/* Removing flash messages */
+function removeFlash() {
+  const element = document.getElementById("div_flash");
+  element.remove();
+}
+
+setTimeout(function() {
+  removeFlash();
+}, 4000);
