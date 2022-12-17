15b70d0aac0100618b54d42da3c35afc9f3d980b
zootedb0t <05kraken@proton.me>
2022-12-17 14:00:04 +0530

    closes #6

--- shortner/__init__.py
+++ shortner/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import secrets
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 # adding configuration for using a sqlite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///url.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///url.db"
 db = SQLAlchemy(app)
 
 secret = secrets.token_urlsafe(32)
