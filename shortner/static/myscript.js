4079e2bed8aae0cac805e717ee12a45b90c535d2
stoney <05kraken@proton.me>
2023-01-31 09:13:19 +0530

    update: how static files are linked

--- shortner/static/myscript.js
+++ shortner/static/myscript.js
@@ -3,17 +3,3 @@ function urlCopy(e) {
   const data = document.querySelector(e);
   cb.writeText(data.innerText).then(() => alert("URL copied"));
 }
-
-function copyfromdatabase(vars) {
-  // var dummy = document.createElement("textarea");
-  // to avoid breaking orgain page when copying more words
-  // cant copy when adding below this code
-  // dummy.style.display = 'none'
-  // document.body.appendChild(dummy);
-  //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
-  // dummy.value = text;
-  // dummy.select();
-  // navigator.clipboard.writeText(vars);
-  // document.body.removeChild(dummy);
-  console.log(vars)
-}
